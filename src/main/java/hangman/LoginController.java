84ee4fd1c17bd008997ae4bd654e9df984795b57
woxane <mohsen.minavand1384@gmail.com>
2024-06-04 20:35:05 +0330

    Implement shake function in LoginController.java

--- src/main/java/hangman/LoginController.java
+++ src/main/java/hangman/LoginController.java
@@ -3,6 +3,7 @@ package hangman;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.util.Duration;
@@ -34,6 +35,17 @@ public class LoginController {
         usernameTextField.setStyle(css);
         passwordTextField.setStyle(css);
         credentialLabel.setVisible(true);
+    }
+
+
+    public void shake(Node node) {
+        TranslateTransition translateTransition = new TranslateTransition(Duration.millis(50) , node);
+
+        translateTransition.setByX(10f);
+        translateTransition.setCycleCount(4);
+        translateTransition.setAutoReverse(true);
+
+        translateTransition.playFromStart();
     }
 
 }
