fffd1762bbce030ce9c615399bad6357f88d7ec1
woxane <mohsen.minavand1384@gmail.com>
2024-06-04 21:12:51 +0330

    Implement setWrongUsernameStyle function in SignUpController.java file

--- src/main/java/hangman/SignUpController.java
+++ src/main/java/hangman/SignUpController.java
@@ -32,6 +32,15 @@ public class SignUpController {
     }
 
 
+    public void setWrongUsernameStyle() {
+        String css = "-fx-background-radius: 20;-fx-background-color: #181818;-fx-border-color: " +
+                "red;-fx-border-radius: 20;-fx-border-width: 1.6;-fx-text-inner-color: white";
+        usernameTextField.setStyle(css);
+        usernameWarningLabel.setVisible(true);
+
+        shake(usernameTextField);
+    }
+
     public void shake(Node node) {
         TranslateTransition translateTransition = new TranslateTransition(Duration.millis(50) , node);
 
