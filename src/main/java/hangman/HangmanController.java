ddd16f13b949b325bc9cb78335bedc0359b63e12
woxane <mohsen.minavand1384@gmail.com>
2024-06-05 12:15:05 +0330

    Implement handleKeyPressed function in HangmanController.java file

--- src/main/java/hangman/HangmanController.java
+++ src/main/java/hangman/HangmanController.java
@@ -1,14 +1,45 @@
 package hangman;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
 
 public class HangmanController {
     @FXML
-    private Label welcomeText;
-
+    private ImageView stickBase;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView stickStanding;
+    @FXML
+    private ImageView stickUp;
+    @FXML
+    private ImageView Head;
+    @FXML
+    private ImageView Body;
+    @FXML
+    private ImageView rightHand;
+    @FXML
+    private ImageView leftHand;
+    @FXML
+    private ImageView rightLeg;
+    @FXML
+    private ImageView leftLeg;
+
+    private int stage = 0;
+    private final int FINAL_STAGE = 9;
+
+
+    public void handleKeyPressed(javafx.scene.input.KeyEvent event) {
+       if (checkInputChar(event.getCode().getName() , nameOfObject) == false) {
+           stage += 1;
+
+            if (stage == FINAL_STAGE) {
+                showLostScreen();
+            } else {
+                showNextStage(stage);
+            }
+       }
+
+        KeyCode keyCode = event.getCode();
+        System.out.println("Key Pressed: " + keyCode.getName());
     }
 }
