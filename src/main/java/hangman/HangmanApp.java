6b67df05d12cc2aed32b9b515ee62ca40930e243
woxane <mohsen.minavand1384@gmail.com>
2024-06-05 15:49:54 +0330

    add static account and databaseManager value in hangmanApp clas

--- src/main/java/hangman/HangmanApp.java
+++ src/main/java/hangman/HangmanApp.java
@@ -3,14 +3,17 @@ package hangman;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HangmanApp extends Application {
+    public static Account account;
+    public static DatabaseManager databaseManager;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HangmanApp.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HangmanApp.class.getResource("Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 700);
         stage.setTitle("Hangman");
         stage.setScene(scene);
