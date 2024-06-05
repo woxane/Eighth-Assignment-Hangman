df1d24e1c4d390f43036e39ed9816665ee803d70
woxane <mohsen.minavand1384@gmail.com>
2024-06-05 22:56:25 +0330

    Create Leaderboard class to use in TableView javafx

--- src/main/java/hangman/Leaderboard.java
+++ src/main/java/hangman/Leaderboard.java
@@ -1,2 +1,29 @@
-package hangman;public class Leaderboard {
+package hangman;
+
+public class Leaderboard {
+    private String username;
+    private String time;
+    private int wrongGusses;
+    private String word;
+
+    public Leaderboard(String username , String time , int wrongGusses , String word) {
+        this.username = username;
+        this.time = time;
+        this.wrongGusses = wrongGusses;
+        this.word = word;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getTime() {
+        return time;
+    }
+    public int getWrongGusses() {
+        return wrongGusses;
+    }
+
+    public String getWord() {
+        return word;
+    }
 }
