82513e75010de7e521837fbd4525205e0b144473
woxane <mohsen.minavand1384@gmail.com>
2024-06-05 13:11:36 +0330

    implement game class with two constructor in diffrence situation

--- src/main/java/hangman/Game.java
+++ src/main/java/hangman/Game.java
@@ -1,2 +1,29 @@
-package hangman;public class Game {
+package hangman;
+
+import java.util.UUID;
+
+public class Game {
+    private final UUID GAME_ID;
+    private final UUID USER_ID;
+    private final String text;
+    private int wrongGuesses;
+    private int time;
+    private boolean win;
+
+
+    public Game(UUID gameId, UUID userId, String text , int wrongGuesses , int time , boolean win) {
+        this.GAME_ID = gameId;
+        this.USER_ID = userId;
+        this.text = text;
+        this.wrongGuesses = wrongGuesses;
+        this.time = time;
+        this.win = win;
+    }
+
+
+    public Game(UUID gameId, UUID userId, String text) {
+        this.GAME_ID = UUID.randomUUID();
+        this.USER_ID = UUID.randomUUID();
+        this.text = text;
+    }
 }
